22e2ba4b0759568824d04f7e9440309c4531c55c
Gdaj <gdajmatapato@gmail.com>
2019-04-26 05:24:49 +0900

    mapaaaaa

---
+++ www/components/comandos.js
@@ -0,0 +1,46 @@
+$(document).on("click", "#btnMapa", function(){
+  
+  
+
+  var onSuccess = function(position){
+        L.mapquest.key = 'Tb4mQvnfzr5SSkAldnGNtLUzzpQTaaaL';
+        var map = L.mapquest.map('map', {
+          center: [position.coords.latitude, position.coords.longitude],
+          layers: L.mapquest.tileLayer('map'),
+          zoom: 12
+        });   
+
+        map.addControl(L.mapquest.control());
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess);
+
+  checkConnection();
+});
+
+
+
+//Verificar conexão;
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    if(states[networkState] == states[Connection.NONE])
+    {
+
+      navigator.notification.alert("Sem conexão. Por favor, conectar a uma rede.", semConexao, "Alerta", "fechar");
+    }
+    else
+    {
+      navigator.notification.beep(1);
+    }
+}
